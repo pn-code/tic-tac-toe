6b49f024902918ac03e2cb1fd277376acd571fd4
Philip Nguyen <philipnguyen953@gmail.com>
2022-08-15 13:24:35 -0700

    added click functionality

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
+let player = "Player 1"
+
+//Gameboard Module
 const gameBoard = (function (){
     'use strict';
 
     const gameBoard = []
+    const displayPlayer = document.querySelector("#display-player")
+    const squares = Array.from(document.querySelectorAll(".square"))
 
+    squares.map(square => {
+    square.addEventListener('click', (e) => {
+        if (player === "Player 1"){
+            e.target.innerText = "O"
+            player = "Player 2"
+            displayPlayer.innerText = "Player 2"
+            displayPlayer.style.color = "blue"
+            
+        }else if (player === "Player 2"){
+            e.target.innerText = "X"
+            player = "Player 1"
+            displayPlayer.innerText = "Player 1"
+            displayPlayer.style.color = "red"
+        }
+        })
+    });
+    
+
+
+    return {squares}
 
 })();
+
+
 
 
 const displayController = (function(){
